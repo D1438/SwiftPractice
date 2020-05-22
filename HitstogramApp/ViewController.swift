7f9f2d1044aee579cff952674f9b65b0dc165277
I.D <daisuke.workin@gmail.com>
2020-05-23 00:56:22 +0900

    initialize

---
+++ HitstogramApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HitstogramApp
+//
+//  Created by 石澤大輔 on 2020/05/23.
+//  Copyright © 2020 石澤大輔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
