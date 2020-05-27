91bdccd01b1424664ff492ca1af1bbc850e16936
I.D <daisuke.workin@gmail.com>
2020-05-27 21:54:25 +0900

    SelectPopUp maked

---
+++ HitstogramApp/ChartsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ChartsViewController.swift
+//  HitstogramApp
+//
+//  Created by 石澤大輔 on 2020/05/27.
+//  Copyright © 2020 石澤大輔. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class ChartsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- HitstogramApp/ViewController.swift
+++ HitstogramApp/ViewController.swift
@@ -8,13 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate,  UIImagePickerControllerDelegate{
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var pictureImage: UIImageView!
+    
+    @IBAction func selectButtonAction(_ sender: Any) {
+//        カメラかフォトライブラリを選択する
+        let alertController = UIAlertController(title: "確認", message: "選択してください。", preferredStyle: .actionSheet)
+//        カメラが使用可能かチェック
+        if  UIImagePickerController.isSourceTypeAvailable(.camera) {
+//            カメラの選択肢を追加
+            let cameraAction = UIAlertAction(title: "カメラ", style: .default, handler: {
+                (action) in
+//                カメラ起動
+                let imagePickerController = UIImagePickerController()
+                imagePickerController.sourceType = .camera
+                imagePickerController.delegate = self
+                self.present(self, animated: true, completion: nil)
+            })
+            alertController.addAction(cameraAction)
+        }
+        
+//        フォトライブラリが使用可能かチェック
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+            let photoLibraryAction = UIAlertAction(title: "フォトライブラリ", style: .default, handler: {
+                (action) in
+                let imagePickerController = UIImagePickerController()
+                imagePickerController.sourceType = .photoLibrary
+                imagePickerController.delegate = self
+                self.present(self, animated: true, completion: nil)
+            })
+            alertController.addAction(photoLibraryAction)
+        }
+    }
+    
+    @IBAction func deleteButtonAction(_ sender: Any) {
+    }
+    
 }
 
+ 
